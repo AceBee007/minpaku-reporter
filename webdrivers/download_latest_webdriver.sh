72099b366940847c552dccec307da28cfff8b2ed
AceBee007 <zykgcz@gmail.com>
2023-08-17 10:55:21 +0900

    [update] with multi platform support (linux64/mac64/mac_arm64)

--- webdrivers/download_latest_webdriver.sh
+++ webdrivers/download_latest_webdriver.sh
@@ -1,6 +1,31 @@
+#!/bin/bash
+
+# Get the operating system
+OS=$(uname -s)
+
+# Get the machine architecture
+ARCH=$(uname -m)
+
+# Default architecture
+ARCHITECTURE=""
+
+# Check for Linux x86_64
+if [ "$OS" = "Linux" ] && [ "$ARCH" = "x86_64" ]; then
+    ARCHITECTURE="linux64"
+# Check for Mac Intel
+elif [ "$OS" = "Darwin" ] && [ "$ARCH" = "x86_64" ]; then
+    ARCHITECTURE="mac64"
+# Check for Mac Apple Silicon
+elif [ "$OS" = "Darwin" ] && [ "$ARCH" = "arm64" ]; then
+    ARCHITECTURE="mac_arm64"
+else
+    echo "Unsupported architecture"
+    exit 1
+fi
+
 LATEST_CHROMIUM_WEBDRIVER_VERSION=`curl https://chromedriver.storage.googleapis.com/LATEST_RELEASE`
-LATEST_MAC_ARM64_WEBDRIVER_URL="https://chromedriver.storage.googleapis.com/${LATEST_CHROMIUM_WEBDRIVER_VERSION}/chromedriver_mac_arm64.zip"
-curl -OJ "${LATEST_MAC_ARM64_WEBDRIVER_URL}"
+LATEST_WEBDRIVER_URL="https://chromedriver.storage.googleapis.com/${LATEST_CHROMIUM_WEBDRIVER_VERSION}/chromedriver_${ARCHITECTURE}.zip"
+curl -OJ "${LATEST_WEBDRIVER_URL}"
 ZIPFILE=$(ls | grep -E '.zip' | grep 'chromedriver')
 unzip $ZIPFILE -d tmp
 cp ./tmp/chromedriver ./
